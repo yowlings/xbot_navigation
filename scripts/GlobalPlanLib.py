29d621c0fffb9a2b6c1f4b3d284c096be5717b03
yowlings@gmail.com <yowlings@gmail.com>
2018-03-21 22:41:14 +0800

    global plan class init

---
+++ scripts/GlobalPlanLib.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#coding=utf-8
+"""
+ROS global plan lib, including:
+1.Breadth-first search
+2.Depth-first search
+3.Dijkstra search
+4.A* search
+5.Jump Point Search(JPS)
+
+
+"""
+import PriorityQueue
+import rospy,numpy
+
+
+
+
+
+class GlobalPlan():
+	"""docstring for GlobalPlan"""
+	def __init__(self, arg):
+		
+		
+
+	def map_init():
+		pass
+
+	def Breadth_first(self):
+		pass
+
+
+	def Depth_first():
+		pass
+
+	def Dijkstra():
+		pass
+
+	def Astar():
+		pass
+
+	def JPS():
+		pass
+
+	def get_plan():
+		pass
